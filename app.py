fbb1626bbafa0dbc11258946eb57879995389ad5
nichnikov <a.nichnikov@gmail.com>
2023-06-13 18:06:55 +0300

    refactoring for inference

--- app.py
+++ app.py
@@ -16,17 +16,14 @@ app = FastAPI(title="ExpertBotFastText")
 async def search(data: SearchData):
     """searching etalon by  incoming text"""
     logger.info("searched pubid: {} searched text: {}".format(str(data.pubid), str(data.text)))
-    if data.pubid in [6, 9]:
-        try:
-            logger.info("searched text without spellcheck: {}".format(str(data.text)))
-            result = await classifier.tested_searching(str(data.text), data.pubid, 0.95)
-            return result
-        except Exception:
-            logger.exception("Searching problem with text {} in pubid {}".format(str(data.text), str(data.pubid)))
-            return {"templateId": 0, "templateText": "", "text": str(data.text)}
-    else:
+    try:
+        logger.info("searched text without spellcheck: {}".format(str(data.text)))
+        result = await classifier.searching(str(data.text), data.pubid, 0.95)
+        return result
+    except Exception:
+        logger.exception("Searching problem with text {} in pubid {}".format(str(data.text), str(data.pubid)))
         return {"templateId": 0, "templateText": "", "text": str(data.text)}
 
 if __name__ == "__main__":
     # uvicorn.run(app, host=service_host, port=service_port)
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8090)
